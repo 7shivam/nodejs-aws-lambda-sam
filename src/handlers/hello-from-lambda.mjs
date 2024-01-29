28f7253acdfd15236568fc725dc57daf502fdda2
7shivam <52976222+7shivam@users.noreply.github.com>
2024-01-29 15:39:35 +0530

    Update hello-from-lambda.mjs

--- src/handlers/hello-from-lambda.mjs
+++ src/handlers/hello-from-lambda.mjs
@@ -3,7 +3,7 @@
  */
 export const helloFromLambdaHandler = async () => {
     // If you change this message, you will need to change hello-from-lambda.test.mjs
-    const message = 'Hello from Lambda!, shivam';
+    const message = 'Hello from Lambda!, shivam rathore';
 
     // All log statements are written to CloudWatch
     console.info(`${message}`);
